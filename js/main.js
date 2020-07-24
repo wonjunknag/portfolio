622dbc306601c2e8547a68618040a89f29049b0a
wonjunkang <kwjcomein@gmail.com>
2020-07-25 08:12:31 +0900

    JS: work filtering

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ navbarMenu.addEventListener("click", (event) => {
   if (link == null) {
     return;
   }
-  console.log(event.target.dataset.link);
+  //   console.log(event.target.dataset.link);
   scrollIntoView(link);
 });
 
@@ -49,6 +49,29 @@ document.addEventListener("scroll", () => {
 // Handle click on the "arrow up" button
 arrowUp.addEventListener("click", () => {
   scrollIntoView("#home");
+});
+
+// Projects
+const wordkBtnContainer = document.querySelector(".work__categories");
+const projectContainer = document.querySelector(".work__projects");
+const projects = document.querySelectorAll(".project");
+wordkBtnContainer.addEventListener("click", (e) => {
+  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+  if (filter == null) {
+    return;
+  }
+  projectContainer.classList.add("anim-out");
+  setTimeout(() => {
+    projects.forEach((project) => {
+      console.log(project.dataset.type);
+      if (filter === "*" || filter === project.dataset.type) {
+        project.classList.remove("invisible");
+      } else {
+        project.classList.add("invisible");
+      }
+    });
+    projectContainer.classList.remove("anim-out");
+  }, 300);
 });
 
 function scrollIntoView(selector) {
